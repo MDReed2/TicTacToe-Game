022c87a76f78ac614f52eb3cccd12da8140c02b3
mdreed2 <meshia.d.reed@gmail.com>
2021-10-16 16:31:55 -0700

    winner styles

--- app/game/ui.js
+++ app/game/ui.js
@@ -2,7 +2,7 @@ const player1 = "X"
 const player2 = "O"
 let currentPlayer = ""
 
-const spaces = [null, null, null, null, null, null, null, null, null]
+const spaces = ["", "", "", "", "", "", "", "", ""]
 
 let count = 0
 
@@ -24,8 +24,6 @@ const winningOptions = [
   [box[2], box[4], box[6]]
 ]
 
-// console.log(winningOptions)
-
 const selectBox = function (event) {
   const id = event.target.id
 
@@ -43,8 +41,10 @@ const selectBox = function (event) {
     count++
   }
 
-  if (winningPlayer) {
+  if (winningPlayer()) {
     $("#user-display").text(`${currentPlayer} is the WINNER`)
+
+    return
   }
 
   console.log(spaces)
@@ -53,45 +53,27 @@ const selectBox = function (event) {
 
 
 const winningPlayer = function () {
-  // for (let i = 0; i < winningOptions.length; i++) {
-  //   if (winningOptions[0][i] === ) {}
-  // }
 
-  if (spaces[0][0].innerText === currentPlayer) {
-    if (spaces[1][0].innerText === currentPlayer && spaces[2][0].innerText === currentPlayer) {
-      console.log(currentPlayer + " is the winner")
-    }
-  } else if (spaces[3][0].innerText === currentPlay) {
-    if (spaces[4][0].innerText === currentPlayer && spaces[5][0].innerText === currentPlayer) {
-      console.log(currentPlayer + " is the winner")
-    }
-  } else if (spaces[6][0].innerText === currentPlay) {
-    if (spaces[7][0].innerText === currentPlayer && spaces[8][0].innerText === currentPlayer) {
-      console.log(currentPlayer + " is the winner")
-    }
-  } else if (spaces[0][0].innerText === currentPlay) {
-    if (spaces[3][0].innerText === currentPlayer && spaces[6][0].innerText === currentPlayer) {
-      console.log(currentPlayer + " is the winner")
-    }
-  } else if (spaces[1][0].innerText === currentPlay) {
-    if (spaces[4][0].innerText === currentPlayer && spaces[7][0].innerText === currentPlayer) {
-      console.log(currentPlayer + " is the winner")
-    }
-  } else if (spaces[2][0].innerText === currentPlay) {
-    if (spaces[5][0].innerText === currentPlayer && spaces[8][0].innerText === currentPlayer) {
-      console.log(currentPlayer + " is the winner")
-    }
-  } else if (spaces[0][0].innerText === currentPlay) {
-    if (spaces[4][0].innerText === currentPlayer && spaces[8][0].innerText === currentPlayer) {
-      console.log(currentPlayer + " is the winner")
-    }
-  } else if (spaces[2][0].innerText === currentPlay) {
-    if (spaces[4][0].innerText === currentPlayer && spaces[6][0].innerText === currentPlayer) {
-      console.log(currentPlayer + " is the winner")
+  for (let idx = 0; idx < 8; idx++) {
+    if (
+      spaces[winningOptions[idx][0]].innerText ===
+        spaces[winningOptions[idx][1]].innerText &&
+      spaces[winningOptions[idx][1]].innerText ===
+        spaces[winningOptions[idx][2]].innerText &&
+      spaces[winningOptions[idx][2]].innerText ===
+        spaces[winningOptions[idx][0]].innerText &&
+      spaces[winningOptions[idx][0]] != ""
+    ) {
+      return $("#user-display").text = `${
+        spaces[winningOptions[idx][0]].innerText
+      } is the winner`
     }
   }
+  return
 }
 
+
 module.exports = {
-  selectBox
+  selectBox,
+  winningPlayer
 }
